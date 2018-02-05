66efd76749da1eb7a778f2d3eec17cd71895a6a9
goreleaserbot <goreleaser@carlosbecker.com>
2018-02-05 07:18:40 -0500

    bashful version v0.0.11

--- bashful.rb
+++ bashful.rb
@@ -1,9 +1,9 @@
 class Bashful < Formula
   desc ""
   homepage ""
-  url "https://github.com/wagoodman/bashful/releases/download/v0.0.10/bashful_0.0.10_darwin_amd64.tar.gz"
-  version "0.0.10"
-  sha256 "454b5c90edbd879871d15bb12a32bfbfe072175313cc3f00dd79cb8bc20ff824"
+  url "https://github.com/wagoodman/bashful/releases/download/v0.0.11/bashful_0.0.11_darwin_amd64.tar.gz"
+  version "0.0.11"
+  sha256 "75bf6b1bbe525a0ae0c993e5a2237ab7224d9a559b00f7cd845b7eba73d7f529"
 
   def install
     bin.install "bashful"
