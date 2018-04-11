56ddb344a953496f8f0e6b9ce886f23ed7a29b8e
goreleaserbot <goreleaser@carlosbecker.com>
2018-04-10 21:29:21 -0400

    Brew formula update for bashful version v0.1.1

--- bashful.rb
+++ bashful.rb
@@ -1,15 +1,11 @@
 class Bashful < Formula
   desc ""
   homepage ""
-  url "https://github.com/wagoodman/bashful/releases/download/v0.0.12/bashful_0.0.12_darwin_amd64.tar.gz"
-  version "0.0.12"
-  sha256 "b1bc9811f9c87a2bf8b92083f67ae4100f7c5116b11e8b89577f94d77c0da3d1"
+  url "https://github.com/wagoodman/bashful/releases/download/v0.1.1/bashful_0.1.1_darwin_amd64.tar.gz"
+  version "0.1.1"
+  sha256 "ee71acdcd0eab1911f1ac2ca186f05b8a9813ffd1d82c82d6ba5a891997b9122"
 
   def install
     bin.install "bashful"
-  end
-
-  test do
-    
   end
 end
